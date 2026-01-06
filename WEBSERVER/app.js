1353a491fbcb4567870de3b6c598c364beba5512
rifky-alkatiri17 <rifkyalkatiri17@gmail.com>
2026-01-06 09:45:36 +0800

    modifikasi folder webserver

--- WEBSERVER/app.js
+++ WEBSERVER/app.js
@@ -2,6 +2,20 @@ const http = require('http');
 const fs = require('fs');
 // console.log(http)
 
+const renderFile = (path, obj)=>{
+	fs.readFile(path, (err,hasil)=>{
+			if(err){
+				obj.writeHead(404);
+				obj.write('File Not Found...')
+			}else{
+				obj.write(hasil)
+			}
+
+			obj.end();
+		});
+}
+
+//buat server
 const server = http.createServer((req,res)=>{
 	console.log(req.url);	
 	res.writeHead(200,{
@@ -10,31 +24,25 @@ const server = http.createServer((req,res)=>{
 
 	switch(req.url){
 		case '/tes':
-		res.write('<h1 style="color:red">TEST...</h1>');
+		// res.write('<h1 style="color:red">TEST...</h1>');
+		renderFile('index3.html', res);
 		break;
 
 		case '/about':
-		res.write('<h1 style="color:red">ABOUT...</h1>');
+		// res.write('<h1 style="color:red">ABOUT...</h1>');
+		renderFile('index2.html', res);	
 		break;
 
 		default:
 		// res.write('<h1 style="color:red">Hello World...</h1>');
-		fs.readFile('./index1.html', (err,hasil)=>{
-			if(err){
-				res.writeHead(404);
-				res.write('File Not Found...')
-			}else{
-				res.write(hasil)
-			}
-
-			res.end();
-		});
+		renderFile('index1.html', res);
 		break;
 	}
 	
 	
 });
 
+//jalankan server
 server.listen(3000, ()=>{
 	console.log('Server Aktif di port 3000...')
 })
