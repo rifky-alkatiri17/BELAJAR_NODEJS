d10c17158b23601bae958293009d2c71b1c95ef6
rifky-alkatiri17 <rifkyalkatiri17@gmail.com>
2026-01-08 15:34:05 +0800

    menambahkan express-ejs-layouts:main-layout.ejs

--- EXPRESS TEMPLATE ENGINE - EJS/app.js
+++ EXPRESS TEMPLATE ENGINE - EJS/app.js
@@ -3,19 +3,27 @@ const app = express();
 const port = 7000;
 // const ejs = require('ejs');
 const fs = require('fs');
+const expressLayouts = require('express-ejs-layouts');
+// file ejs relative terhadap folder views
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
+app.use(expressLayouts);
 
 app.get('/about', (req,res)=>{
 	// res.send('ini adalah halaman about...')
-	res.render('about', {title:'Halaman About'})
+	res.render('about', {
+		layout: 'layouts/main-layout',
+		title:'Halaman About'
+	})
 });
 
 app.get('/contact', (req,res)=>{
 	// res.send('ini adalah halaman contact...')
-	res.render('contact', {title:'Halaman Contact'})
+	res.render('contact', {
+		layout: 'layouts/main-layout',
+		title:'Halaman Contact'
+	})
 });
 
 app.get('/product/:id', (req,res)=>{
@@ -25,7 +33,11 @@ app.get('/product/:id', (req,res)=>{
 app.get('/', (req,res)=>{
 	// res.sendFile('index.html',{root:__dirname})
 	fs.readFile('./data.json',{encoding:'utf-8'}, (err,result)=>{
-		res.render('index', {title:'Halaman Index', result: JSON.parse(result)});
+		res.render('index', {
+			layout: 'layouts/main-layout',
+			title:'Halaman Index', 
+			result: JSON.parse(result)
+		});
 		// console.log(result)
 	});
 });
